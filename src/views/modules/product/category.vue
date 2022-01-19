1e77d4a9c712191da871544be7ebbbfd8057392d
sunny <675326787@qq.com>
2022-01-19 21:24:30 +0800

    添加category删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -49,6 +49,14 @@ export default {
       console.log("append", data);
     },
     remove(node, data) {
+      var ids=[data.catId]
+      this.$http({
+        url: this.$http.adornUrl("/product/category/delete"),
+        method: "post",
+        data: this.$http.adornData(ids, false),
+      }).then(({ data }) => {
+        console.log("刪除成功");
+      });
       console.log("remove", node, data);
     },
     handleNodeClick(data) {
